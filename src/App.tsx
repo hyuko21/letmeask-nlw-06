e819c16387e7a5091d49288c630ccd2d34417ae3
Victor Wagner <victorwdr@gmail.com>
2021-06-21 20:31:47 -0300

    feat(pages): add Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,7 @@
-import { Button } from './components/Button';
+import { Home } from './pages/Home';
 
 function App() {
-  return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
