310ab40542b56851ea41ac3f26d68c1c762f1c6e
Victor Wagner <victorwdr@gmail.com>
2021-06-24 13:42:08 -0300

    feat: add closing room button at AdminRoom page

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { database } from '../services/firebase';
 
 import deleteImg from '../assets/images/delete.svg';
@@ -17,10 +17,19 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
+  const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId);
+
+  async function handleCloseRoom() {
+    await database.ref(`rooms/${roomId}`).update({
+      closedAt: new Date(),
+    });
+
+    history.push('/');
+  }
 
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Tem certeza que deseja excluir esta pergunta?')) {
@@ -35,7 +44,9 @@ export function AdminRoom() {
           <img src={logoImg} alt='Letmeask' />
           <div>
             <RoomCode code={roomId} />
-            <Button isOutlined>Encerrar sala</Button>
+            <Button isOutlined onClick={handleCloseRoom}>
+              Encerrar sala
+            </Button>
           </div>
         </div>
       </header>
